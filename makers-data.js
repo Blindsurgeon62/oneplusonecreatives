fe012cac999987f45263eb0c19d8d0f53f328ec9
Blindsurgeon62 <blindsurgeon62@gmail.com>
2025-11-30 02:59:23 -0800

    trying again, sans security uopdate

---
+++ makers-data.js
@@ -0,0 +1,20 @@
+// Makers Showcase Data for Homepage
+const makersData = [
+    {
+        name: 'Edward "word" Dizon',
+        title: 'Rapper, Poet, Author',
+        image: 'makersshowcase/Ed-Herolink1.jpg',
+        link: 'https://linktr.ee/word707',
+        blurb: 'I worked with this dude a couple years ago. He really is about, what it is he writes about. The authenticity and impartiality he brings to every interaction, combines with his internal drive to create, really made me want to reach out while building my page. Thanks dude!'
+    },
+    {
+        name: 'Josh Eppenauer',
+        title: 'Filmmaker, Composer',
+        image: 'makersshowcase/Josh-Herolink1.jpg',
+        link: 'https://www.josheppenauer.com/',
+        blurb: 'I knew Josh and his dad years back, and I recently got back in touch with them. Seeing the stellar guy Josh has become was a gift I didn\'t know I needed. He is such a talented filmmaker and composer whose work is genuinely fantastic. Looking forward to seeing what he creates next!'
+    }
+];
+
+// Homepage feature blurb
+const makersFeatureText = "In the time I've had in this life so far, I've been blessed through meetings with lots of great people. This is my chance to share some of them with you! These are folks that are making things that matter to them - people who inspired me with how they took the step past thinking about something into actually doing. They're proof that you don't need permission to start creating. You just need to start.";
---
+++ tinkering-data.js
@@ -0,0 +1,43 @@
+// Tinkering/Restoration Projects Data for Homepage
+const tinkeringData = [
+    {
+        title: "Bokken Restoration",
+        type: "video",
+        thumb: "videos/Bokken.Pt1.mp4", // Using first video as thumbnail
+        videos: [
+            "videos/Bokken.Pt1.mp4",
+            "videos/Bokken.Pt2.mp4",
+            "videos/Bokken.Pt3.mp4"
+        ],
+        description: "Restoring a wooden training katana using calligraphy ink as wood stain",
+        projectUrl: "tinkering.html#bokken",
+        date: "November 6, 2025"
+    },
+    {
+        title: "AV-1 Light Seal Replacement",
+        type: "video",
+        thumb: "videos/CanonAV-1 Lightseal pt.1.mp4",
+        videos: ["videos/CanonAV-1 Lightseal pt.1.mp4"],
+        description: "Learning how to film restoration videos while fixing camera light seals",
+        projectUrl: "tinkering.html#av1",
+        date: "November 6, 2025"
+    },
+    {
+        title: "Fun with Scissors",
+        type: "video",
+        thumb: "videos/guillotine.mp4",
+        videos: ["videos/guillotine.mp4"],
+        description: "A fool-proof and totally safe negative cutting solution",
+        projectUrl: "tinkering.html#scissors",
+        date: "October 17, 2025"
+    },
+    {
+        title: "Shoulder Strap Sew Time",
+        type: "image",
+        thumb: "slrphotos/CanonstrapThumb011.jpg",
+        images: ["slrphotos/CanonstrapThumb011.jpg"],
+        description: "Making a vintage Canon strap work with a less-vintage camera",
+        projectUrl: "shoulderstrapsewtime.html",
+        date: "June 11, 2025"
+    }
+];
